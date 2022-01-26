c6c5b5ea46ba4a8066641eef718c266eff1942eb
Marco Napetti <nappa85@therocktrading.com>
2022-01-26 09:44:20 +0100

    Serve empty paths

--- src/main.rs
+++ src/main.rs
@@ -22,22 +22,30 @@ pub struct Device {
     pub last_seen: i64,
 }
 
+async fn get_all_instances() -> Result<impl warp::Reply, Infallible> {
+    get_instances(String::new()).await
+}
+
 async fn get_instances(instances: String) -> Result<impl warp::Reply, Infallible> {
     let instances = instances.replace("%20", " ");
-    let instances = instances.split(',').collect::<Vec<_>>();
+    let instances = if instances.is_empty() { vec![] } else { instances.split(',').collect::<Vec<_>>() };
     let data = cache::INSTANCES.load();
     let temp = data.iter()
-        .filter_map(|o| instances.contains(&o.name.as_str()).then(|| o))
+        .filter_map(|o| (instances.is_empty() || instances.contains(&o.name.as_str())).then(|| o))
         .collect::<Vec<_>>();
     Ok(warp::reply::json(&temp))
 }
 
+async fn get_all_devices() -> Result<impl warp::Reply, Infallible> {
+    get_devices(String::new()).await
+}
+
 async fn get_devices(instances: String) -> Result<impl warp::Reply, Infallible> {
     let instances = instances.replace("%20", " ");
-    let instances = instances.split(',').collect::<Vec<_>>();
+    let instances = if instances.is_empty() { vec![] } else { instances.split(',').collect::<Vec<_>>() };
     let data = cache::DEVICES.load();
     let temp = data.iter()
-        .filter_map(|o| instances.contains(&o.instance_name.as_deref().unwrap_or_default()).then(|| o))
+        .filter_map(|o| (instances.is_empty() || instances.contains(&o.instance_name.as_deref().unwrap_or_default())).then(|| o))
         .collect::<Vec<_>>();
     Ok(warp::reply::json(&temp))
 }
@@ -50,7 +58,9 @@ async fn main() {
 
     let routes = warp::get()
         .and(warp::path!("instances" / String).and_then(get_instances))
-        .or(warp::path!("devices" / String).and_then(get_devices));
+        .or(warp::path!("instances").and_then(get_all_instances))
+        .or(warp::path!("devices" / String).and_then(get_devices))
+        .or(warp::path!("devices").and_then(get_all_devices));
 
     warp::serve(routes)
         .run(([0, 0, 0, 0], 9991))
